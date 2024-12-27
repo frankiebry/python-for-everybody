c46c2422979c7904314a27dda44406cf9b4646f0
Frankie <34449029+usagibryan@users.noreply.github.com>
2024-12-27 09:46:12 -0500

    Update pay program with function

---
+++ test/test.py
@@ -0,0 +1,22 @@
+# def print_lyrics():
+#     print("I'm a lumberjack, and I'm okay.")
+#     print('I sleep all night and I work all day.')
+    
+# def repeat_lyrics():
+#     print_lyrics()
+#     print_lyrics()
+    
+# repeat_lyrics()
+
+# def print_twice(bruce):
+#     print(bruce)
+#     print(bruce)
+#     return('Done')
+    
+# result = print_twice("Carbon")
+# print(result)
+
+def add_two(a,b):
+    return(a+b)
+
+print(add_two(100,1))
