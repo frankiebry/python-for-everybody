14250fe6fc097626c533ac461b8993e321aed42c
Frankie <34449029+usagibryan@users.noreply.github.com>
2025-01-01 12:35:16 -0500

    Update README

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 My solutions to the exercises from the book Python for Everybody.
+
+# TODO
+- [ ] Organize folders by chapter, and put .txt files used in both chapter 7 and 8 in the same folder. Programs that use this files should then have updated paths.
---
+++ files/min_email_client.py
@@ -0,0 +1,13 @@
+# Exercise 5 from Chapter 8 of the book "Python for Everybody" by Charles Severance
+
+fhand = open('files/mbox-short.txt')
+
+count = 0
+for line in fhand:
+    words = line.split()
+    # print('Debug:', words)
+    if len(words) < 2 or words[0] != 'From' : continue
+    print(words[1])
+    count += 1
+    
+print('There were', count, 'lines in the file with From as the first word')
